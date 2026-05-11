10dd60823bdcc678c1a4e587cff37e4d8ac5169c
Lynnov <boaboo@126.com>
2026-05-11 16:39:16 +0800

    fix: complete tauri desktop build setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running TomatoBuddy");
+    tomato_buddy_lib::run();
 }
